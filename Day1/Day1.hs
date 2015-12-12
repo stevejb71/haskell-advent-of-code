4105b9d65a463f65e10d43e71ddcd3e2964e28c1
Stephen Bastians <stevejb71@gmail.com>
2015-12-12 22:28:23 +0000

    Day 1 part 2

--- Day1/Day1.hs
+++ Day1/Day1.hs
@@ -2,10 +2,16 @@ module Main where
 
 import Data.List (foldl')
 
-main :: IO ()
-main = readFile "input.txt" >>= print . calcFloor
+part1 :: IO ()
+part1 = readFile "input.txt" >>= print . last . calcFloor
 
-calcFloor :: String -> Int
-calcFloor s = foldl' step 0 s
+part2 :: IO ()
+part2 = readFile "input.txt" >>= print . reachesBasementPosition
+
+reachesBasementPosition :: String -> Int
+reachesBasementPosition = length . takeWhile (/= -1) . calcFloor
+
+calcFloor :: String -> [Int]
+calcFloor s = scanl step 0 s
   where step c '(' = c + 1
         step c ')' = c - 1
